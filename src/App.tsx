3c50e38abe2bc3ccf7d4ef8ea1d0d9950d4bbac3
Cristiano Passos <passosdarezzo@hotmail.com>
2022-12-26 11:35:57 -0300

    Adicionado a funcionalidade de seleção de cores

--- src/App.tsx
+++ src/App.tsx
@@ -2,24 +2,29 @@ import React, { useState, useEffect } from 'react';
 import './App.css';
 import Ponto, { IPonto } from './components/Ponto';
 
-
 function App() {
   const [listaPontos, setListaPontos] = useState<IPonto[]>([]);
   const [refazer, setRefazer] = useState<IPonto[]>([]);
+  const [corPonto, setCorPonto] = useState<string>("#2a33b2");
+  const [corTela, setCorTela] = useState<string>("#ffffff");
 
   useEffect(() => {
     window.document.title = "Click-Dots";
   }, []);
 
   const geraPonto = (event: React.MouseEvent<HTMLElement>) => {
-    console.log(`X: ${event.clientX} - Y: ${event.clientY}`);
-
     const ponto = {
       x: event.clientX - 10,
-      y: event.clientY - 10
+      y: event.clientY - 10,
+      cor: corPonto
     }
 
     setListaPontos(prev => [...prev, ponto]);
+
+    // Limpa a lista de refazer
+    if (refazer.length > 0) {
+      setRefazer([]);
+    }
   }
 
   const desfazer = (event: React.MouseEvent<HTMLElement>) => {
@@ -44,38 +49,76 @@ function App() {
     event.stopPropagation();
     const ponto = refazer.pop();
 
-    if(ponto !== undefined) {
+    if (ponto !== undefined) {
       setListaPontos(prev => [...prev, ponto]);
     }
+  }
+
+  const defineCorTela = (event: React.ChangeEvent<HTMLInputElement>) => {
+    event.stopPropagation();
+    setCorTela(event.target.value);
+  }
+
+  const defineCorPonto = (event: React.ChangeEvent<HTMLInputElement>) => {
+    event.stopPropagation();
+    setCorPonto(event.target.value);
   }
 
   return (
     <div
       className="page"
+      style={{
+        background: `${corTela}`
+      }}
       onClick={geraPonto}
     >
-      <button
-        onClick={desfazer}
-      >
-        Desfazer
-      </button>
-      <button
-        onClick={onRefazer}
-      >
-        Refazer
-      </button>
-      <button
-        onClick={limpar}
-      >
-        Limpar
-      </button>
+      <div className="toolbox">
+        <div>
+          <input
+            id="tela"
+            type="color"
+            name="tela"
+            value={corTela}
+            onClick={(event) => event.stopPropagation()}
+            onChange={defineCorTela}
+          />
+          <label htmlFor="tela">&nbsp;&nbsp;Tela</label>
+        </div>
 
+        <div>
+          <input
+            id="ponto"
+            type="color"
+            name="ponto"
+            value={corPonto}
+            onClick={(event) => event.stopPropagation()}
+            onChange={defineCorPonto}
+          />
+          <label htmlFor="ponto">&nbsp;&nbsp;Ponto</label>
+        </div>
+        <button
+          onClick={desfazer}
+        >
+          Desfazer
+        </button>
+        <button
+          onClick={onRefazer}
+        >
+          Refazer
+        </button>
+        <button
+          onClick={limpar}
+        >
+          Limpar
+        </button>
+      </div>
       {
         listaPontos.map((p, idx) => (
           <Ponto
             key={idx}
             x={p.x}
             y={p.y}
+            cor={p.cor}
           />
         ))
       }
--- src/components/Ponto/index.tsx
+++ src/components/Ponto/index.tsx
@@ -3,15 +3,17 @@ import React from "react";
 export interface IPonto {
   x: number
   y: number
+  cor?: string
 }
 
-const Ponto = ({ x, y }: IPonto) => {
+const Ponto = ({ x, y, cor }: IPonto) => {
   return (
     <span
       className="dot"
       style={{
         top: y + "px",
-        left: x + "px"
+        left: x + "px",
+        backgroundColor: cor
       }}
     />
   );
